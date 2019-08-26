9b9676bd2c0db56262fb77d09dd08dadd70b6b94
sreejapk <sreejapkaran@gmail.com>
2019-08-26 17:15:24 +0530

    Add files via upload

---
+++ SpellCheck_W2W.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+
+
+# In[16]:
+
+
+import gensim
+model = gensim.models.KeyedVectors.load_word2vec_format('/home/sreeja/data/GoogleNews-vectors-negative300.bin', 
+                                                        binary=True)
+
+words = model.index2word
+
+w_rank = {}
+for i,word in enumerate(words):
+    w_rank[word] = i
+
+WORDS = w_rank
+print("done")
+
+
+# In[19]:
+
+
+#model = gensim.models.Word2Vec()
+model = gensim.models.KeyedVectors.load_word2vec_format('/home/sreeja/data/GoogleNews-vectors-negative300.bin', 
+                                                        binary=True)
+model.most_similar('word')
+print("done")
+
+
+# In[4]:
+
+
+import re
+from collections import Counter
+
+def words(text): return re.findall(r'\w+', text.lower())
+
+def P(word): 
+    "Probability of `word`."
+    # use inverse of rank as proxy
+    # returns 0 if the word isn't in the dictionary
+    return - WORDS.get(word, 0)
+
+def correction(word): 
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word): 
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+def known(words): 
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word): 
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+
+# In[6]:
+
+
+correction('adres')
+
+
+# In[27]:
+
+
+print(model.most_similar('mising'))
+print("done")
+
+
+# In[ ]:
+
+
+
+
